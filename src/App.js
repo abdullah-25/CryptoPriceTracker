b8f28902f856048097612604c0d4aa7eb5f697bb
abdullah-25 <70604820+abdullah-25@users.noreply.github.com>
2022-01-08 01:58:46 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./styles.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Coin from "./coin";
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+export default function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a crypto coin</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="search"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.total_volume}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            marketCap={coin.market_cap}
+          />
+        );
+      })}
+    </div>
+  );
+}
